ac3c1ff4299e158755e25523414cc3ca6320e02d
Dawa Ometto <d.l.a.ometto1@uu.nl>
2023-09-19 12:06:19 +0200

    Allow configuring a workspace definition.

--- deploy.py
+++ deploy.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import yaml
 import json
@@ -9,15 +9,25 @@ from optparse import OptionParser
 
 parser = OptionParser()
 parser.add_option("-m", "--method", choices=("ansible", "docker"),
-                  help="Which method to use to execute ansible on the target (ansible into the target, or use docker exec).", default="ansible")
+                  help="Which method to use to execute ansible on the target (ansible into the target, or use docker exec).",
+                  default="ansible")
+parser.add_option("-c", "--config",
+                  help="Path to .yml file describing the components and parameters to be deployed.")
 (options, args) = parser.parse_args()
+
+if not options.config:
+    raise Exception("You did not provide the required -c option.")
 
 method = options.method
 
+with open(options.config) as cfg:
+    workspace_config = yaml.safe_load(cfg)
+
+default_dir = workspace_config['default_script_dir']
+
 DOCKER_IMG_NAME = 'src-basic-workspace'
 CONTAINER_NAME = 'src-test-container'
-CONFIG = 'test-components.yml'
-EXTERNAL_COMPONENT = '/Users/dawa/Code/uu/src/deploy_components/plugin-external-plugin/plugin-external-plugin.yml'
+EXTERNAL_COMPONENT = './plugin-external-plugin/plugin-external-plugin.yml'
 
 def execute_ansible(container, plugin):
     ansible_cmd = 'ansible-playbook -b -u root -c docker -i "{container}," -vvvv --extra-vars {parameters} {playbook}'
@@ -27,18 +37,13 @@ def execute_ansible(container, plugin):
     return os.system(cmd)
 
 def execute_docker(container, plugin):
-    cmd =  "ansible-playbook --connection=local -b {remote_plugin_arguments} --extra-vars="{remote_plugin_parameters}" /rsc/plugins/{remote_plugin_script_folder}/{remote_plugin_path}".format(
-        remote_plugin_arguments = plugin.arguments
-        remote_plugin_parameters = plugin.parameters
-        remote_plugin_script_folder = os.path.basename(plugin.script_folder)
+    cmd =  'ansible-playbook --connection=local -b {remote_plugin_arguments} --extra-vars="{remote_plugin_parameters}" /rsc/plugins/{remote_plugin_script_folder}/{remote_plugin_path}'.format(
+        remote_plugin_arguments = plugin.arguments,
+        remote_plugin_parameters = plugin.parameters,
+        remote_plugin_script_folder = os.path.basename(plugin.script_folder),
         remote_plugin_path = plugin.path
     )
     return container.exec_run(cmd=cmd, priviliged=true)
-
-with open(CONFIG) as cfg:
-    components_cfg = yaml.safe_load(cfg)
-
-default_dir = components_cfg['default_script_dir']
 
 print('Starting up test image...')
 client = docker.from_env()
@@ -46,7 +51,7 @@ container = client.containers.run(DOCKER_IMG_NAME, detach=True, command="/bin/ba
 print('Container started -- do not forget to stop it! Container name: {}'.format(container.name))
 
 try:
-    for component in components_cfg['enabled_components']:
+    for component in workspace_config['components']:
         extra_vars = {
             'remote_plugin':
             {
@@ -58,9 +63,9 @@ try:
                 'arguments': '-i 127.0.0.1,'
             }
         }
-        if method is 'ansible':
+        if method == 'ansible':
             result = execute_ansible(container, plugin)
-        elif method is 'docker':
+        elif method == 'docker':
             result = execute_docker(container, plugin)
         else:
             raise Exception("Unknown method: {}".format(METHOD))
