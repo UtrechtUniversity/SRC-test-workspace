998041ab5ea72cf02d5d844a02d0272d07456cde
Dawa Ometto <d.l.a.ometto1@uu.nl>
2023-09-28 09:59:49 +0200

    Allow setting architecture with ARCH env variable

--- pack.sh
+++ pack.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 IMG="ubuntu20"
 VALID_SOURCES="docker, vagrant"
+
 if [ "$#" -lt 1 ]
 then
   echo "No valid build targets supplied. Pass at least one of: $VALID_SOURCES."
   exit 1
 fi
+
+if [ -z $ARCH ]
+then
+  ARCH="linux/amd64"
+else
+  ARCH="linux/$ARCH"
+fi
+echo "Building image for architecture: $ARCH"
 
 DOCKER_SRC="\"sources.docker.ubuntu\""
 VAGRANT_SRC="\"sources.vagrant.ubuntu\""
@@ -30,6 +39,6 @@ else
   printf -v joined_targets '%s,' "${TARGETS[@]}"
 fi
 
-CMD="packer fmt $IMG && packer build -var 'enabled_sources=[$joined_targets]' $IMG"
+CMD="packer fmt $IMG && packer build -var 'enabled_sources=[$joined_targets]' -var 'target_arch=$ARCH' $IMG"
 echo "Running: $CMD"
 eval "$CMD";
